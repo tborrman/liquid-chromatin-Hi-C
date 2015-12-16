9fb0ad43a9d91406d9369fa28b4b1f53863e5562
tborrman <tylerborrman@gmail.com>
2015-12-16 16:02:05 -0500

    1 bp breaks

--- dpnII_abs_frag_distance_hist.R
+++ dpnII_abs_frag_distance_hist.R
@@ -11,7 +11,7 @@ distances <- scan(paste(wkdir, args$f, sep="/"))
 
 png(paste(wkdir, "/", args$f, ".png", sep=""), width=4000, height=2000, res=300)
 par(mar=c(5, 5, 4, 2) + 0.1)
-hist(distances, breaks=20000, xlim=c(0, 100), main="HBCRACKHiC-K562-DN-TD-R1",
+hist(distances, breaks=seq(0,max(distances)), xlim=c(0, 100), main="HBCRACKHiC-K562-DN-TD-R1",
 	ylab="# of mapped fragments", xlab="bp distance to nearest DpnII site (GATC)", cex.lab=1.5, cex.main=1.5)
 box(bty="l")
 dev.off()
