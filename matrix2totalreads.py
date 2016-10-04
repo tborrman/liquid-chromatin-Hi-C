3aa12da5d547a824f3a3632e773317eb97ee6ab9
tborrman <tylerborrman@gmail.com>
2016-10-03 20:10:42 -0400

    upper tri

--- matrix2totalreads.py
+++ matrix2totalreads.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 import argparse
 import numpy as np
+import gzip
 
-parser=argparse.ArgumentParser(description='Input a matrix and output count of total reads')
+parser=argparse.ArgumentParser(description='Input a matrix and output count of total interactions in upper triangle exluding diagonal')
 parser.add_argument('-i', help='Input matrix', type=str, dest='i', required=True)
 args=parser.parse_args()
 
+if args.i[-3:] == '.gz':
+	IN = gzip.open(args.i, 'rb')
+else:
+	IN = open(args.i, 'r')
 # Count lines to skip in header
-IN = open(args.i, 'r')
 counter=0
 for line in IN:
     if '#' in line:
         counter += 1
 
 x=np.genfromtxt(args.i, comments='#', delimiter='\t', skip_header=counter + 1, missing_values='nan')
-y = np.nansum(x, axis=1)
+up_tri = np.triu(x,1)
+y = np.nansum(up_tri, axis=1)
 print 'Total reads: ' + str(np.nansum(y))
 
 
