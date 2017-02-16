9ae5730e2be1ba96cd32b95a996ec84a231219d0
tborrman <tylerborrman@gmail.com>
2017-02-16 18:31:55 -0500

    hot ones

--- HOT_scale.py
+++ HOT_scale.py
@@ -2,17 +2,19 @@
 import argparse
 import sys
 import os
+import subprocess
 import shutil
+import re
 import h5py
 import matrix_functions as mf
 
 
 parser=argparse.ArgumentParser(description='Houda Tyler scaling: scale matrices such that ' +
-	' allxall = billion total reads; ice matrices; scale again. Must be run in a resolution ' + 
-	'directory from resulting Dekker mapping pipeline (ex. HiC_library/C-10000000/).')
-parser.add_argument('-p', help='sample prefix before __hdf or __txt (ex. HBCRACKHiC-K562-MN-R1__hg19)',
+	' allxall = billion total reads; ice matrices; scale again. Must be run on hdf5 matrices. ' +
+	'balance.py for icing step must be in PATH' )
+parser.add_argument('-i', help='input matrix hdf5 format (HBCRACKHiC-K562-DN__hg19__genome__C-10000000-raw.hdf5)',
 	type=str, required=True)
-parser.add_argument('-t', help="type of files: 'matrix' or 'hdf'", type=str, required=True)
+
 args = parser.parse_args()
 
 
@@ -23,9 +25,6 @@ def all_x_all_total_reads(file):
 	if file[-5:] == '.hdf5':
 		x = mf.hdf5_2_numpy_matrix(file)
 		total = mf.total_reads(x)
-	elif file[-10:] == '.matrix.gz':
-		x, colnames, rownames = mf.dekker_2_numpy_matrix(file)
-		total = mf.total_reads(x)
 	else:
 		print 'ERROR: wrong file extension'
 		sys.exit()
@@ -35,8 +34,8 @@ def scaleMatrixbyFactor(file, factor):
 	# HDF5 format
 	if file[-5:] == '.hdf5':
 		filename_prefix = file[:-5]
-		shutil.copy('raw/' + file, filename_prefix + '_scaleMatrixbyFactor_' + str(factor) + '.hdf5')
-		f = h5py.File(filename_prefix + '_scaleMatrixbyFactor_' + str(factor) + '.hdf5', 'r+')
+		shutil.copy('raw/' + file, filename_prefix + '_scaleBy_' + str(round(factor,2)) + '.hdf5')
+		f = h5py.File(filename_prefix + '_scaleBy_' + str(round(factor,2)) + '.hdf5', 'r+')
 		scaled_interactions = f['interactions'][:] * factor
 		blocksize = f['interactions'].chunks
 		del f['interactions']
@@ -44,18 +43,8 @@ def scaleMatrixbyFactor(file, factor):
 		f.close()
 		
 	else:
-		if file[-10:] == '.matrix.gz':
-			filename_prefix = file[:-10]
-		elif file[-7:] == '.matrix':
-			filename_prefix = file[:-7]
-		else:
-			print 'ERROR: not .matrix or .hdf5 file'
-			quit()
-
-		X, xheader, yheader = mf.dekker_2_numpy_matrix('raw/' + file)
-		scaled_X = X*factor
-		mf.numpy_matrix_2_dekker(scaled_X, xheader, yheader, filename_prefix + '_scaleMatrixbyFactor_' + str(factor) + '.matrix.gz')
-
+		print 'ERROR: wrong file extension'
+		sys.exit()
 
 def main():
 
@@ -63,42 +52,24 @@ def main():
 		os.makedirs('hot')
 	###########################################################
 	# Step 1. Scale 
-	# Check if allxall matrix exits
-	if args.t == 'matrix':
-		ext = 'matrix.gz'
-	elif args.t == 'hdf':
-		ext = 'hdf5'
-	else:
-		print 'ERROR: incorrect file type'
-		sys.exit()
-	allxall = '../C-10000000/raw/' + args.p + '__genome__C-10000000-raw.' + ext
-	try:
-		open(allxall, 'r')
-	except IOError as e:
-		print 'File does not exist'
-		print e
-		sys.exit()
+
 	# Get total reads of allxall matrix
-	total_reads = all_x_all_total_reads(allxall)
+	total_reads = all_x_all_total_reads(args.i)
 	scale_factor = 1000000000.0 / total_reads
 
-	# Scale all files
-	for file in os.listdir('raw'):
-		if file[-5:] == '.hdf5' or file[-10:] == '.matrix.gz' or file[-7:] =='.matrix':
-			scaleMatrixbyFactor(file, scale_factor)
+	# Scale file
+	scaleMatrixbyFactor(args.i, scale_factor)
+	scaled_file = args.i[:-5] + '_scaleBy_' + str(round(scale_factor, 2)) + '.hdf5'
 
+	# Ice file
+	p = subprocess.Popen(['balance.py', '-i', scaled_file, '-v'])
+	p.wait()
+	iced_file = scaled_file[:-5] + '.balanced.hdf5'
 
-	
-
-
-
-
-
-
-
-	
-
-
+	# Rescale iced matrix
+	total_reads = all_x_all_total_reads(iced_file)
+	scale_factor = 1000000000.0 / total_reads
+	scaleMatrixbyFactor(iced_file, scale_factor)
 
 
 if __name__ == '__main__':
