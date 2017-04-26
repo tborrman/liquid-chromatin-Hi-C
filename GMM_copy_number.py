ab5338be65a35c7b40775ee0d495a084a8518892
tborrman <tylerborrmant@gmail.com>
2017-04-26 18:19:32 -0400

    still working

--- GMM_copy_number.py
+++ GMM_copy_number.py
@@ -7,26 +7,46 @@ parser=argparse.ArgumentParser(description='Fit a Gaussian Mixture Model to the 
 parser.add_argument('-d', help='DpnII signal bed file (ex. dpnII_coverage_sorted.bed)', type=str, required=True, dest='d')
 args=parser.parse_args()
 
-dpnII = np.genfromtxt(args.d, delimiter='\t', usecols=3)
-# Remove outlier peaks
+def main():
 
-dpnII = dpnII[(dpnII <= 3500) & (dpnII >= 500)]
+	dpnII_chr = np.genfromtxt(args.d, delimiter='\t', usecols=(0), dtype=str)
+	dpnII = np.genfromtxt(args.d, delimiter='\t', usecols=(1,2,3))
 
-dpnII = dpnII.reshape((len(dpnII),1))
-dpnII = dpnII
+	# Remove outlier peaks
+	dpnII_chr = dpnII_chr[(dpnII[:,2] <= 3500) & (dpnII[:,2] >= 500)]
+	dpnII = dpnII[(dpnII[:,2] <= 3500) & (dpnII[:,2] >= 500)]
+	dpnII_reads = dpnII[:,2]
+	dpnII_reads = dpnII_reads.reshape((len(dpnII_reads),1))
 
-g=mixture.GMM(n_components=3)
-g.fit(dpnII)
+	g=mixture.GMM(n_components=3, tol=0.0001)
+	g.fit(dpnII_reads)
 
-print "converged"
-print np.round(g.converged_,2)
-print "weights"
-print np.round(g.weights_, 2)
-print "means"
-print np.round(g.means_, 2)
-print "covaraiance"
-print np.round(g.covars_, 2)
-print "score_samples"
-print len(g.score_samples(dpnII))
+	print "converged"
+	print g.converged_
+	print "weights"
+	print np.round(g.weights_, 2)
+	print "means"
+	print np.round(g.means_, 2)
+	print "covariance"
+	print np.round(g.covars_, 2)
+
+	# Write posterior probabilities output file
+	np.set_printoptions(suppress=True)
+	means = np.round(g.means_, 2)
+	probs = g.score_samples(dpnII_reads)[1]
+	OUT=open('GMM_post_probs.txt', 'w')
+	OUT.write('\t\t\t\t' + str(means[0,0]) +'\t'+ str(means[1,0]) +'\t'+ str(means[2,0]) + '\n')
+	for i in range(len(dpnII)):
+		OUT.write('\t'.join([dpnII_chr[i], str(int(dpnII[i,0])), str(int(dpnII[i,1])), 
+			str(int(dpnII[i,2])), '{:.5f}'.format(probs[i][0]),
+			'{:.5f}'.format(probs[i][1]),
+			'{:.5f}'.format(probs[i][2])])
+			 + '\n')
+	OUT.close()
+	
+if __name__ == '__main__':
+	main()
+
+
 
 
