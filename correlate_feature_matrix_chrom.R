1c35f6f4252354c16cf032c54c3139dfa44869d3
U-DESKTOP-61UO44U\Tyler <tborrman@gmail.com>
2018-02-17 14:43:24 -0500

    single chrom

---
+++ correlate_feature_matrix_chrom.R
@@ -0,0 +1,26 @@
+library(corrplot)
+library(pheatmap)
+library(RColorBrewer)
+df <- read.table("../feature_matrix.txt", sep="\t", header=TRUE)
+hl_df <- data.frame()
+for (chrom in c(paste("chr", 1:22, sep=""), "chrX")) {
+  chrom_df <- df[df$chrom == chrom,]
+  features <- chrom_df[,4:ncol(chrom_df)]
+  c <- as.data.frame(cor(features, use="pairwise.complete.obs"))
+  hl_chr <-c[1,2:ncol(c)]
+  rownames(hl_chr) <- chrom
+  hl_df <- rbind(hl_df, hl_chr)
+  
+  # png(paste('correlate_feature_matrix_', chrom,'.png', sep=""),width=2500, height=2500, res=300)
+  # corrplot(c, method="circle",type = "upper", tl.col = "black")
+  # dev.off()
+  # 
+  # png(paste('correlate_feature_matrix_values_', chrom,'.png', sep=""),width=2500, height=2500, res=300)
+  # corrplot(c, method="number", type="upper", tl.col = "black", number.cex = 0.5)
+  # dev.off()
+}
+
+png("correlate_feature_matrix_half_life_chrom.png", width=2500, height=2500, res=300)
+  pheatmap(hl_df,color=rev(colorRampPalette(rev(brewer.pal(n = 7, name ="RdYlBu")))(100)))
+dev.off()
+  
