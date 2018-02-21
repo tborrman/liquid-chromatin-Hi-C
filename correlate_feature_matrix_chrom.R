d9237fa2d499ca8808de5e677bd84dc5d3cc0f26
U-DESKTOP-61UO44U\Tyler <tborrman@gmail.com>
2018-02-20 19:45:26 -0500

    z-scores

--- correlate_feature_matrix_chrom.R
+++ correlate_feature_matrix_chrom.R
@@ -3,6 +3,12 @@ library(pheatmap)
 library(RColorBrewer)
 df <- read.table("../feature_matrix.txt", sep="\t", header=TRUE)
 hl_df <- data.frame()
+
+z_score <- function(x) {
+  z <- (x - mean(x, na.rm=TRUE))/ (sd(x, na.rm=TRUE))
+  return(z)
+}
+
 for (chrom in c(paste("chr", 1:22, sep=""), "chrX")) {
   chrom_df <- df[df$chrom == chrom,]
   features <- chrom_df[,4:ncol(chrom_df)]
@@ -11,6 +17,17 @@ for (chrom in c(paste("chr", 1:22, sep=""), "chrX")) {
   rownames(hl_chr) <- chrom
   hl_df <- rbind(hl_df, hl_chr)
   
+  # z-score chrom matrices
+  z_chrom_df <- data.frame(chrom_df[1:3], apply(chrom_df[4:ncol(chrom_df)], 2, z_score))
+  # Remove LADs
+  z_chrom_df <- z_chrom_df[-c(24,25)]
+  # Transpose
+  z_chrom_df <- t(as.matrix(z_chrom_df[4:ncol(z_chrom_df)]))
+  
+  png(paste("z-score_features_", chrom, ".png",sep=""), width=6000, height=2500, res=300)
+  pheatmap(z_chrom_df,color=rev(colorRampPalette(rev(brewer.pal(n = 7, name ="RdYlBu")))(100)),
+           cluster_cols=FALSE, labels_col = "")
+  dev.off()
   # png(paste('correlate_feature_matrix_', chrom,'.png', sep=""),width=2500, height=2500, res=300)
   # corrplot(c, method="circle",type = "upper", tl.col = "black")
   # dev.off()
