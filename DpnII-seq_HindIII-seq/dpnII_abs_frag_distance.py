b87360160cfd04944f23ef63a26c26e9461d2e21
tborrman <tylerborrman@gmail.com>
2018-03-13 01:14:12 -0400

    output file option

--- DpnII-seq_HindIII-seq/dpnII_abs_frag_distance.py
+++ DpnII-seq_HindIII-seq/dpnII_abs_frag_distance.py
@@ -7,8 +7,26 @@ sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
 parser = argparse.ArgumentParser(description='Write list of distances to closest DpnII site for fragment '+
 	'(distance reported is the minimum distance from either fragment end)')
-parser.add_argument('-s', help='SAM file with alignment reads (bowtie_output_mapped.sam)', type=str, dest='s', required=True)
+parser.add_argument('-s', help='SAM file with alignment reads (bowtie_output_mapped.sam)', type=str, required=True)
+parser.add_argument('-o', help='output zero-distance and nonzero-distance fragment files', type= bool, default= False)
 args = parser.parse_args()
+
+def write_distance_files(l1, l2, Zf, nonZf, min_d):
+	'''
+	Write fragments into files with zero distance to dpnII site
+	fragments and nonzero distance to dpnII site fragments
+
+	Args:
+		l1: string read 1 (SAM line)
+		l2: string read 2 (SAM line)
+		Zf: filehandle zero distance fragments
+		nonZf: filehandle non zero distance fragments
+		min_d: minumum distance to dpnII site for fragment
+	'''
+	if min_d == 0:
+		Zf.write(l1 + l2)
+	else:
+		nonZf.write(l1 + l2)
 
 def main():
 
@@ -16,7 +34,7 @@ def main():
 	dpnII_sites = {}
 	chroms = map(str, range(1,23)) + ['X','Y']
 	for chrom in chroms:
-		DPN = open('../dpnII/dpnII_sites_chr'+chrom+'.bed', 'r')
+		DPN = open('/home/tb37w/project/Research/digest/dpnII/test/hg19/dpnII/dpnII_sites_chr'+chrom+'.bed', 'r')
 		coords = []
 		for line in DPN:
 			# Change coordinates from BED file format
@@ -32,6 +50,9 @@ def main():
 	SAM = open(args.s, 'r')
 	index = args.s[-3:]
 	OUT = open('dpnII_abs_frag_distances_'+index,'w')
+	if args.o:
+		ZERO_DIST_OUT = open('zero_distance_'+index, 'w')
+		NONZERO_DIST_OUT = open('nonzero_distance_'+index, 'w')
 	counter = 0
 	while True:
 		line1=SAM.readline()
@@ -47,14 +68,14 @@ def main():
 			chrom2 = line2.split()[2]
 			if chrom1 != chrom2:
 				print 'ERROR: wrong chromosomes'
-				quit()
+				sys.exit()
 			# Ignore mitochondrial DNA
 			if chrom1 == 'chrM':
 				continue
 			# Check alignment has pair
 			if (int(line1.split()[8]) < 0) or (int(line2.split()[8]) > 0):
 				print 'ERROR: no pair'
-				quit()
+				sys.exit()
 			# Get start and end of fragment
 			start = int(line1.split()[3])
 			end = int(line2.split()[3]) + 99
@@ -63,9 +84,14 @@ def main():
 			closest_end = min(dpnII_sites[chrom1], key=lambda x: abs(end-x))
 			# Minimum distance to dpnII sites from either end
 			min_distance = min(abs(start-closest_start), abs(end-closest_end))
+			if args.o:
+				write_distance_files(line1, line2, ZERO_DIST_OUT, NONZERO_DIST_OUT, min_distance)
 			OUT.write(str(min_distance) + '\n')
 	SAM.close()
 	OUT.close()
+	if args.o:
+		ZERO_DIST_OUT.close()
+		NONZERO_DIST_OUT.close()
 
 if __name__ == '__main__':
 	main()
