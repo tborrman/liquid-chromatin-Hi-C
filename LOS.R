ca7588bb542722051fd7ee53d23d9369179eb7eb
tborrman <tylerborrman@gmail.com>
2018-03-18 18:36:54 -0400

    LOS adjust

--- LOS.R
+++ LOS.R
@@ -3,15 +3,13 @@
 library(argparse)
 options(scipen=100)
 parser <- ArgumentParser(description="input cis percent timepoint and return LOS")
-parser$add_argument('-i', help='input cis percent timepoint file (ex. *.cistransratio.bedGraph', type="character",
+parser$add_argument("-i", help="input cis percent timepoint file (ex. *_cispercent.bedGraph)", type="character",
                     required=TRUE)
+parser$add_argument("-m", help="Mock nuclei control cispercent.bedGraph", type="character", required=TRUE)
 args <- parser$parse_args()
-wkdir <- getwd()
 
-M_cis_df <- read.table(paste(wkdir, "HBHiC-K562-MN-Dp-1__hg19__genome__C-500000-raw_scaleBy_2.72.balanced_scaleBy_51.45.cistransratio.bedGraph", sep="/"), 
-                       sep="\t", skip=1)
-D_cis_df <- read.table(paste(wkdir, args$i, sep="/"), 
-                       sep="\t", skip=1)
+M_cis_df <- read.table(args$m, sep="\t", header=FALSE)
+D_cis_df <- read.table(args$i, sep="\t", header=FALSE)
 # Fix bp indexing
 M_cis_df[2] <- M_cis_df[2] - 1
 D_cis_df[2] <- D_cis_df[2] - 1
@@ -20,9 +18,9 @@ D_cis_df[2] <- D_cis_df[2] - 1
 colnames(M_cis_df) <- c("chrom", "start", "end", "M_cis")
 colnames(D_cis_df) <- c("chrom", "start", "end", "D_cis")
 
+
 # Merge M_cis and D_cis dfs
 cis_df <- merge(M_cis_df, D_cis_df, c("chrom","start"))
-
 # Compute structure metric
 LOS <- round((cis_df$M_cis - cis_df$D_cis)/cis_df$M_cis,5)
 cis_df <- cbind(cis_df, LOS)
@@ -39,7 +37,7 @@ out_df$chrom <- factor(out_df$chrom, levels=c(paste("chr", 1:22, sep=""), "chrX"
 # Order
 out_df <- out_df[order(out_df$chrom, out_df$start),]
 
-write.table(out_df, paste(substr(args$i, 1, nchar(args$i) - 23), '.LOS.bedGraph', sep="")
+write.table(out_df, paste(substr(args$i, 1, nchar(args$i) - 20), '_LOS.bedGraph', sep="")
             , row.names=FALSE, sep="\t", quote=FALSE)
 
 
