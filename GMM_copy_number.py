157f7bc2f5c72b28e44ce258ba8475b073926acb
tborrman <tylerborrman@gmail.com>
2018-03-29 17:55:15 -0400

    column for copy number states (ploidy)

--- GMM_copy_number.py
+++ GMM_copy_number.py
@@ -44,7 +44,31 @@ def remove_outliers(d, min_reads, max_reads):
 	idx_keep = np.reshape(idx_keep, (len(idx_keep), 1))
 	f = np.append(d, idx_keep, axis=1)
 	return f
+
+def get_copynumber_states(mu, p):
+	'''
+	Assign copy number states based on posterior 
+	probabilites of each mixture component for 
+	each observation
+	Args: 
+		mu: np.array of means for mixture
+			compononets
+		p: np.array of posterior probabilites
+	Returns:
+		c: list of copy number states 
+	'''
 	
+	sort_mu = np.sort(mu)
+	copy_number = np.array([np.where(sort_mu == x)[0][0] for x in mu]) + 2
+	# Get copy number state
+	c = []
+	for row in p:
+		if np.all(np.isnan(row)):
+			c.append(np.nan)
+		else:
+			max_idx = np.argmax(row)
+			c.append(copy_number[max_idx])
+	return c
 
 
 def main():
@@ -71,23 +95,27 @@ def main():
 	print "covariance"
 	print np.round(g.covars_, 2)
 
-	# Write posterior probabilities output file
+	
 	np.set_printoptions(suppress=True)
-	means = np.round(g.means_, 2)
+	means = np.round(g.means_, 2).flatten()
 	probs = g.score_samples(dpnII_reads)[1]
 	probs_full = np.zeros((len(dpnII), 3))
 	probs_full[:] = np.nan
 	kept_idx = df[:, 3].astype(int)
 	probs_full[kept_idx, :] = probs
 
+	copynum = get_copynumber_states(means, probs_full)
 
+	# Write copy number results to file
 	OUT=open('GMM_post_probs.txt', 'w')
-	OUT.write('\t\t\t\t' + str(means[0,0]) +'\t'+ str(means[1,0]) +'\t'+ str(means[2,0]) + '\n')
+	OUT.write('chrom\tstart\tend\treads\t' + str(means[0]) +'\t'+ str(means[1]) +
+		'\t'+ str(means[2]) + '\tcopy\n')
 	for i in range(len(dpnII)):
 		OUT.write('\t'.join([dpnII_chr[i], str(int(dpnII[i,0])), str(int(dpnII[i,1])), 
 			str(int(dpnII[i,2])), '{:.5f}'.format(probs_full[i][0]),
 			'{:.5f}'.format(probs_full[i][1]),
-			'{:.5f}'.format(probs_full[i][2])])
+			'{:.5f}'.format(probs_full[i][2]),
+			str(copynum[i])])
 			 + '\n')
 	OUT.close()
 	
