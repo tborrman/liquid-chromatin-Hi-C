243dd8c581126d01ac8cb1e72406a7d5c5178a2b
tborrman <tylerborrman@gmail.com>
2018-09-01 02:14:40 -0400

    more RNA

--- halflife_RNA-seq_density.R
+++ halflife_RNA-seq_density.R
@@ -76,6 +76,7 @@ png("half-life_RNA-seq_total_+_R1_density.png", width=2000, height=1400, res=300
 ggplot(df_hl_RNA_labs, aes(x=hl, color=RNAseq_signal)) +
   geom_density() +
   xlim(0,400) +
+  ylim(0,0.025) +
   xlab(bquote("t"[1/2] ~ "(minutes)")) +
   ggtitle("total RNA-seq (+) strand") + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
@@ -95,6 +96,39 @@ ggplot(df_hl_RNA_labs, aes(x=hl, color=RNAseq_signal)) +
   xlim(0,400) +
   xlab(bquote("t"[1/2] ~ "(minutes)")) +
   ggtitle("total RNA-seq (-) strand") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+dev.off()
+
+####################################################################################
+# A compartment only
+# RNA-seq_total_+ density plot
+df_NA <- df[c("half-life_LOS", "RNA-seq_total_+_R1", "PCA_eigen1")]
+colnames(df_NA) <- c("hl", "RNAseq", "PC1")
+df_hl_RNA <- na.omit(df_NA)
+RNAseq_signal <- stratify_RNAseq(df_hl_RNA$RNAseq)
+df_hl_RNA_labs <- cbind(df_hl_RNA, RNAseq_signal)
+df_A <- df_hl_RNA_labs[df_hl_RNA_labs$PC1 > 0,]
+df_B <- df_hl_RNA_labs[df_hl_RNA_labs$PC1 <= 0,]
+
+png("half-life_RNA-seq_total_+_R1_density_Acomp.png", width=2000, height=1400, res=300)
+ggplot(df_A, aes(x=hl, color=RNAseq_signal)) +
+  geom_density() +
+  xlim(0,400) +
+  ylim(0,0.025) +
+  xlab(bquote("t"[1/2] ~ "(minutes)")) +
+  ggtitle("total RNA-seq (+) strand A compartment") + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+dev.off()
+
+png("half-life_RNA-seq_total_+_R1_density_Bcomp.png", width=2000, height=1400, res=300)
+ggplot(df_B, aes(x=hl, color=RNAseq_signal)) +
+  geom_density() +
+  xlim(0,400) +
+  ylim(0,0.025) +
+  xlab(bquote("t"[1/2] ~ "(minutes)")) +
+  ggtitle("total RNA-seq (+) strand B compartment") + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black"))
 dev.off()
