503251a9f74aac556f240e1798be548a318dd088
tborrman <tylerborrman@gmail.com>
2018-11-30 00:19:15 -0500

    remove dat shit

--- gmm4lecture.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import numpy as np 
-from sklearn import mixture
-data = np.genfromtxt(args.d, delimiter='\t', usecols=3)
-mino, maxo = get_outlier_cutoffs(data)
-dpnII_reads = remove_outliers(data, mino, maxo)
-dpnII_reads = dpnII_reads.reshape((len(dpnII_reads),1))
-g=mixture.GMM(n_components=3)
-g.fit(dpnII_reads)
