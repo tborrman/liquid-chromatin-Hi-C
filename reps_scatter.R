c737b29607613fcba683acef23b848cd4c6a17fc
tborrman <tylerborrman@gmail.com>
2019-01-13 01:18:53 -0500

    reps_scatter.R

--- reps_scatter.R
+++ reps_scatter.R
@@ -1,11 +1,12 @@
-#!/bin/env/Rscript
+#!/usr/bin/env Rscript
 library(RColorBrewer)
 library(argparse)
 
-parser <- ArgumentParser(description="Make smooth scatter plot for input matrices")
-parser$add_argument("-x", help="Input matrix for x-axis", type="character", dest="x", required=TRUE)
-parser$add_argument("-y", help="Input matrix for y-axis", type="character", dest="y", required=TRUE)
+parser <- ArgumentParser(description="Make smooth scatter plot for input replicate matrices")
+parser$add_argument("-x", help="Input matrix for Rep1 on x-axis", type="character", dest="x", required=TRUE)
+parser$add_argument("-y", help="Input matrix for Rep2 on y-axis", type="character", dest="y", required=TRUE)
 parser$add_argument("-o", help="outfile", type="character", dest="o", required=TRUE)
+parser$add_argument("-t", help="title", type="character", dest="t", required=TRUE)
 args <- parser$parse_args()
 
 x_df <- read.table(gzfile(args$x), header=TRUE, row.names=1)
@@ -14,14 +15,19 @@ y_df <- read.table(gzfile(args$y), header=TRUE, row.names=1)
 x <- as.vector(t(as.matrix(x_df)))
 y <- as.vector(t(as.matrix(y_df)))
 
-png(args$o, height=2500, width=2500, res=300)
-smoothScatter(x, y,
-              xlab=args$x, ylab=args$y, col='white', cex=1.25,
-              colramp=colorRampPalette(rev(brewer.pal(11,'Spectral'))))
-dev.off()
+logx <- log2(x)
+logy <- log2(y)
 
-png(paste("log_", args$o, sep=""), height=2500, width=2500, res=300)
-smoothScatter(log2(x), log2(y),
-              xlab=args$x, ylab=args$y, col='white', cex=1.25,
+logx[!is.finite(logx)] <- NA
+logy[!is.finite(logy)] <- NA
+
+pdf(args$o, height=10, width=10)
+par(mar=c(6, 5, 4, 2) + 0.1)
+smoothScatter(logx, logy,
+              xlab="Replicate 1 log2 interactions", ylab="Replicate 2 log2 interactions", 
+              col='white', cex=1.25, main=args$t, cex.axis=1.5, cex.lab=1.5, cex.main=1.5,
               colramp=colorRampPalette(rev(brewer.pal(11,'Spectral'))))
+text(4,15, 
+	sprintf("r = %.3f", round(cor(x, y, method="pearson", use="complete.obs"),3)), 
+		col="white", cex=2)
 dev.off()
