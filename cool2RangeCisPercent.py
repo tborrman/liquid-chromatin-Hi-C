e49ab175f053a4b23d1be4aa25b538dc2af100ff
sergpolly <sergpolly@gmail.com>
2019-10-25 15:58:52 -0400

    functional cool2RangeCisPercent.py - to be tested though

--- cool2RangeCisPercent.py
+++ cool2RangeCisPercent.py
@@ -13,6 +13,7 @@ parser=argparse.ArgumentParser(description='Get cis percent for window range of 
 ' for each row of Hi-C matrix')
 parser.add_argument('-i', help='input cooler Hi-C file or cooler URI', type=str, required=True)
 parser.add_argument('-r', help='window range', type=int, default=6000000)
+parser.add_argument('-d', help='number of diagonals to ignore', type=int, default=0)
 parser.add_argument('-n', help='maximum percent of NAs allowed in window', type=float, default=10.0)
 args=parser.parse_args()
 
@@ -80,37 +81,112 @@ def _get_chunk_coverage_cis(chunk, pixel_weight_key='count'):
     # and beware the main diagonal double counting
     return cov
 
+def _get_mask(n_bins,
+            max_nan,
+            bias,
+            chromnames,
+            chrom_offsets,
+            cis_range_size):
+    """
+    put calculations of the mask into a function
+
+    n_bins - number of bins - size of the mask ultimately
+    max_nan - number of NaNs allowed within cis_range on each row
+    bias - balancing weights with NaNs for filtered out rows/cols
+    chromnames - list of chromosome names
+    chrom_offsets - a function that returns chromosome offests
+    cis_range_size - a half-width cisRange size
+
+    Return:
+    --------------------------------
+    mask - boolear array with n_bins valuess
+    """
+
+    # we will create a mask to fill cisRange
+    # with NaNs where mask is True:
+
+    # initialize with everyhting maked:
+    mask = np.ones(n_bins,dtype=np.bool)
+
+    # (1) mask grange of each chrom's start and end
+    chrom_pairs = zip(chromnames[:-2],chromnames[1:-1])
+    chrom_offsets = [ (chrom_offsets(chr1)+cis_range_size, \
+                       chrom_offsets(chr2)-cis_range_size) \
+                    for chr1,chr2 in chrom_pairs]
+    # unmask the middle of each chromosome:
+    for _1,_2 in chrom_offsets:
+        mask[_1:_2] = False
+    
+    # (2) rows/cols filtered by balancing should be masked
+    mask[np.isnan(bias)] = True
+
+    # (3) mask rows/cols with too many NaNs in cisRange:
+    # count # of NaNs in a given cisRange for a given row
+    # can be easily computed by using a sliding window of
+    # size of cisRange and counting NaNs there: 
+    kernel_size = 2 * cis_range_size + 1
+    count_NaN_kernel = np.ones(kernel_size, dtype='int')
+    numnans_cisRange = np.convolve(
+                            np.isnan(bias),
+                            count_NaN_kernel,
+                            mode='same')
+    mask[numnans_cisRange > max_nan] = True
+
+    # the mask is ready and is full compliant with the Tyler's
+    # if,then,else implementation
+    return mask
+
+
 def main():
 
-    # percent of NAs allowed
+    # percent of NANs allowed
     p = args.n/100
+    NaNsfraction = p
 
-    clr = cooler.Cooler(args.i)
+    # how many diagonals to ignore
+    ignore_diags = args.d
 
-
-    binsize = clr.info['bin-size']
-    n_bins = clr.info['nbins']
+    # genomic range - /2 to account for
+    # upstream/downstream counting:
     grange = args.r/2
+
+    # init a cooler object
+    path_uri = args.i
+    clr = cooler.Cooler(path_uri)
+
+    # introducing binsize:
+    binsize = clr.info['bin-size']
     range_bins=int(grange/binsize)
-    # chunksize
 
-    # for the cooler-ones ...
-    max_NAs = ((tyler_grange/binsize) * NaNsfraction)
+    # Number of NaN-row/cols we allow
+    # a given CisRange band to intersect:
+    max_NAs = 2 * range_bins * NaNsfraction
 
-    # cooler-specific ones ...
-    ignore_diags = 0
+    # bins, number of bins, balancing weight, etc.
+    n_bins = clr.info['nbins']
     clr_bins = clr.bins()[:]
     bias = clr_bins['weight'].values
-    # should we turn NaNs to 0-s ?
-    _bias = np.nan_to_num(bias)
+
+    # we will create a mask to fill cisRange
+    # with NaNs where mask is True:
+    mask = _get_mask(n_bins,
+                     max_NAs,
+                     bias,
+                     clr.chromnames,
+                     clr.offset,
+                     range_bins)
+    # the mask is ready and is full compliant with the Tyler's
+    # if,then,else implementation
+
 
     # start with actual calculations,
-    # replacing "cp = mf.get_cis_percent_range(f, args.r, p)" ...
+    # replacing "cp = mf.get_cis_percent_range(f, args.r, p)":
 
+    # filling NaNs in the balancing weights with 0-s:
+    _bias = np.nan_to_num(bias)
     # generate a stream of pixels (split into chunks) and apply various
     chunks = cooler.tools.split(clr, chunksize=int(1e7), map=map, use_lock=False)
     chunks = chunks.pipe(_zero_diags, n_diags=ignore_diags) if ignore_diags>0 else chunks
-
     cov = (
         chunks
             .pipe(_zero_range, range_bins=range_bins)
@@ -130,33 +206,38 @@ def main():
             .pipe(_get_chunk_coverage_cis, pixel_weight_key="balanced")
             .reduce( np.add, np.zeros(n_bins) )
     )
-
     # afterwards covs-diag/2 should be the same as clr.maxtrix()[:].sum(axis=0) ...
-
-    chrom_pairs = zip(clr.chromnames[:-2],clr.chromnames[1:-1])
-    offsets = [(clr.offset(chr1)+range_bins, clr.offset(chr2)-range_bins) for chr1,chr2 in chrom_pairs]
 
     # some final  modifications we need to do
     # to the cooler-pipeline generated result
     # to make it indistinguishable from Tyler's:
-
     cp = (cov-diag/2)
-    cp[cov==0] = np.nan
-    cp[tyler_nans > max_NAs] = np.nan
+    cp[mask] = np.nan
+    clr_bins['cisRange'] = cp
 
-    # to be rewritten ...
     # Write output
-    OUT = open(args.i[:-5] + '_range' + str(args.r/1000000) + 'Mb_cispercent.bedGraph', 'w')
-    # Only using 22 autosomes and X
-    y_chrom_bin_start =  f['chr_bin_range'][:][23][0]
-    for i, b in enumerate(f['bin_positions'][:][:y_chrom_bin_start]):
-        if b[0] == 22:
-            chrom = 'chrX'
-        else:
-            chrom = 'chr' + str(b[0]+1)
-        OUT.write(chrom+'\t'+str(b[1])+'\t'+str(b[2])+
-        '\t'+ str(cp[i])+'\n')
-    OUT.close()
+    # strip cool/mcool and uri part from input fname:
+    cfname = path_uri.split("::")[0]
+    cfname = '.'.join(cfname.split(".")[:-1])
+    OUT_fname = cfname + '_range' + str(int(2*grange/1000000)) + 'Mb_cispercent.bedGraph'
+    # Only using 22 autosomes and X ...
+    clr_bins.to_csv(OUT_fname,
+                    sep='\t',
+                    na_rep='NA',
+                    columns=["chrom","start","end",'cisRange'],
+                    header=True,
+                    index=False)
+
+    # # something about excluding chrY chrM and other stuff
+    # y_chrom_bin_start =  f['chr_bin_range'][:][23][0]
+    # for i, b in enumerate(f['bin_positions'][:][:y_chrom_bin_start]):
+    #     if b[0] == 22:
+    #         chrom = 'chrX'
+    #     else:
+    #         chrom = 'chr' + str(b[0]+1)
+    #     OUT.write(chrom+'\t'+str(b[1])+'\t'+str(b[2])+
+    #     '\t'+ str(cp[i])+'\n')
+    # OUT.close()
 
 
 if __name__ == '__main__':
