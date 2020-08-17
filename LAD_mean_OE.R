5fd4e6bde5c2f4dd235f161f8408a734f59ccce9
borrmant <tylerborrman@gmail.com>
2020-08-16 23:19:03 -0400

    more order

--- LAD_mean_OE.R
+++
@@ -1,8 +0,0 @@
-options(scipen=100)
-# Lamin analysis
-OE <- read.table("C:/cygwin64/home/Tyler/Research/digest/lamina/mmc2/DataS2_Clone.5-5.1N.OE.txt", header=TRUE, sep="\t")
-OE$start <- OE$start - 1
-OE_data <- OE[4:ncol(OE)]
-means <- apply(OE_data, 1, mean)
-OE_means <- cbind(OE[1:3], means)
-write.table(OE_means,"Lamina_mean_OE_Clone.5-5.1N.OE.txt", sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE)
--- heatmap_controller.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import subprocess
-
-p = subprocess.Popen('ls', shell=True, stdout= subprocess.PIPE)
-files = p.communicate()[0].split('\n')
-
-for f in files:
-	subprocess.call("bsub -q short -W 1:00 -R 'rusage[mem=5000]' perl /home/tb37w/project/Research/ENCODE/cworld-dekker/scripts/perl/heatmap.pl " +
-		"-i " + f, shell=True)
